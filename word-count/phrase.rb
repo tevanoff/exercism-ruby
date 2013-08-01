ff3fe74d885d59a48c3aa42abd484ebd0373b395
Todd Evanoff <evanoff@gmail.com>
2013-07-31 23:20:17 -0400

    Using each_with_object.

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -8,8 +8,13 @@ class Phrase
   end
 
   def word_count
-    word_counts = Hash.new(0)
-    text.downcase.scan(/\w+/) { |word| word_counts[word] += 1 }
-    word_counts
+    words.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
+  end
+
+
+  private
+
+  def words
+    text.downcase.scan(/\w+/)
   end
 end
